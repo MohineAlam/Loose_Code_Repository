738db20ac83900705b3d5ac467e7722001f703c1
MohineAlam <mohine.tasrin.alam@gmail.com>
2024-08-05 21:15:37 +0100

    initial commit

---
+++ Bash_Count_Reads.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source /miniconda3/conda.sh
+conda activate QC
+
+# file output
+output_file="~/path/to/output/count_reads.txt"
+
+# initialise the output text file
+echo -n "Total_Reads" > "$output_file"
+
+# loop through each .bam file in your folder
+for bam_file in "~/path/to/output/*.bam"; do
+
+	# message to user
+	echo "Processing $bam_file"
+
+	#get file name without path
+	bam_filename=$(basename "$bam_file")
+
+	# get total reads
+	total_reads=$(samtools view -c "$bam_file")
+	echo "$bam_filename total_reads: $total_reads" >> "$output_file"
+
+	# get mapped reads
+	mapped_reads=$(samtools view -c -F 4 "$bam_file")
+	echo "$bam_filename mapped_reads: $mapped_reads" >> "$output_file"
+
+	# get unmapped reads
+	unmaped_reads=$(samtools view -c -f 4 "$bam_file")
+	echo "$bam_filename unmapped_reads: $unmapped_reads" >> "$output_file"
+done
+
+echo "Processing done. Results are in $output_file"
