8c22d535b44713caf4000a815486c2477904c733
Mohine <166409071+MohineAlam@users.noreply.github.com>
2025-01-27 22:30:58 +0000

    Create human_disease_genetics_basis.py

---
+++ human_disease_genetics_basis.py
@@ -0,0 +1,6 @@
+# Project steps:
+# decide on specific disease or gene you want to focus on
+# gather genetic data-sets
+# set up environement: pandas, matplotlib, biopython, seaborn, requests
+# data process function - retrieve data set, compare gene sequences, look for genetic markers (statistical tests to assess significance)
+# visualise data
