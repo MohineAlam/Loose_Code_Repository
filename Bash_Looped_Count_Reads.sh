9d0102134b765c0cde14157b1255eeaa118eb32b
Mohine <166409071+MohineAlam@users.noreply.github.com>
2025-10-16 13:06:14 +0100

    Update Bash_Looped_Count_Reads.sh

--- Bash_Looped_Count_Reads.sh
+++ Bash_Looped_Count_Reads.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # file output
-output_file=$"$HOME/path/to/output/count_reads.txt"
-input_file=$"$HOME/path/to/input"
+output="$1"
+input="$2"
 # initialise the output text file
-echo -n "Total_Reads" > "$output_file"
+echo -n "Total_Reads" > "$output"/count_reads.txt
 
 # loop through each .bam file in your folder
-for bam_file in "$input_file"*.bam; do
+for bam_file in "$input"/*.bam; do
 
 	# message to user
 	echo "Processing $bam_file"
@@ -17,15 +17,15 @@ for bam_file in "$input_file"*.bam; do
 
 	# get total reads
 	total_reads=$(samtools view -c "$bam_file")
-	echo "$bam_filename total_reads: $total_reads" >> "$output_file"
+	echo "$bam_filename total_reads: $total_reads" >> "$output"/count_reads.txt
 
 	# get mapped reads
 	mapped_reads=$(samtools view -c -F 4 "$bam_file")
-	echo "$bam_filename mapped_reads: $mapped_reads" >> "$output_file"
+	echo "$bam_filename mapped_reads: $mapped_reads" >> "$output"/count_reads.txt
 
 	# get unmapped reads
 	unmaped_reads=$(samtools view -c -f 4 "$bam_file")
-	echo "$bam_filename unmapped_reads: $unmapped_reads" >> "$output_file"
+	echo "$bam_filename unmapped_reads: $unmapped_reads" >> "$output"/count_reads.txt
 done
 
-echo "Processing done. Results are in $output_file"
+echo "Processing done. Results are in $output"
